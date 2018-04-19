5ed8af6728599052f7a4fe8b7fd5cff3075d7492
ElJerry <kanegerardo_@hotmail.com>
2018-04-18 23:46:14 -0500

    Agrego clases para la lectura del CSV asi como una clase para extraer la informacion de cada trade

--- CalculadoraGanancias/src/calculadoraganancias/CalculadoraGanancias.java
+++ CalculadoraGanancias/src/calculadoraganancias/CalculadoraGanancias.java
@@ -11,11 +11,28 @@ package calculadoraganancias;
  */
 public class CalculadoraGanancias {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        LectorCSV lector = new LectorCSV();
+        Contenido contenido = lector.getContenido();
+        
+//        System.out.println("Contenido " + contenido.contenido.size());
+//        System.out.println("Ccantidad " + contenido.contenido.get(1).size());
+//
+//        for(int i=0 ; i<9; i++){
+//            System.out.println("Cantidad " + i + ": " + contenido.contenido.get(i).size());
+//        }
+//        
+//        System.out.println("contenido conflictuoso");
+//        for(String e : contenido.contenido.get(0)){
+//            System.out.print(e + " ");
+//            System.out.println(e.length());
+//        }
+        
+        for(int i=1 ; i<contenido.contenido.get(1).size(); i++){
+            Trade t = new Trade(i, contenido);
+            System.out.println(t);
+        }
     }
     
 }
---
+++ CalculadoraGanancias/src/calculadoraganancias/Contenido.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoraganancias;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gera
+ */
+public class Contenido {
+        public ArrayList<ArrayList<String>> contenido;
+}
